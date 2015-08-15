2e7a92a2eb94eb87213d0ea40186689c6b88bf21
= <sebbekarlsson97@gmail.com>
2015-08-15 11:28:39 +0200

    Database & fix

--- __main__.py
+++ __main__.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from random import randint
+import MySQLdb as mdb
 
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def main():
+    setup_database()
+    app.run()
 
 @app.route('/', methods=["GET"])
 def index():
@@ -13,7 +18,18 @@ def index():
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
-    return render_template('/pages/login.html')
+    error = 'There was an error'
+    return render_template('/pages/login.html', error=error)
+
+def setup_database():
+    con = mdb.connect('localhost', 'root', 'tango255', 'mailsystem');
+
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY AUTO_INCREMENT, mail_id varchar(64), mail_pw varchar(64))")
 
 if (__name__) == '__main__':
-    app.run()
+    main()
+
+
+
+
